5470e5ef1352d2719458e7199531b23b2f04705d
Ekaterina Smirnova <ekaterina.smirnova199@yandex.ru>
2023-10-15 13:43:42 +0300

    making code prettier

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,13 @@
 // @todo: Темплейт карточки
+
 const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
 const cardsList = document.querySelector('.places__list');
+
 // @todo: Функция создания карточки
+
 function createCard(card, deleteFunction) {
     const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
     const cardItemImage = cardItem.querySelector('.card__image');
@@ -18,14 +23,20 @@ function createCard(card, deleteFunction) {
     return cardItem;
 }
 
+// Функция добавления карточки
+
 function addCard(card, list){
     const createdCard = createCard(card, deleteCard);
     list.append(createdCard);
 }
+
 // @todo: Функция удаления карточки
+
 function  deleteCard(evt){
     const cardToDelete = evt.target.closest('.card');
     cardToDelete.remove();
 }
+
 // @todo: Вывести карточки на страницу
+
 initialCards.forEach((card)=>{addCard(card, cardsList)});
